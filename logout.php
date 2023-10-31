ab27c7029f90f578ecb5c5851a3cb490d1349d62
Anna <annagirl312@gmail.com>
2023-10-31 13:34:06 +0300

    first commit

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+    //Запускаем сессию
+    session_start();
+ 
+    unset($_SESSION["Email"]);
+    unset($_SESSION["Пароль"]);
+     
+    // Возвращаем пользователя на ту страницу, на которой он нажал на кнопку выход.
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: ".$_SERVER["HTTP_REFERER"]);
+?>
